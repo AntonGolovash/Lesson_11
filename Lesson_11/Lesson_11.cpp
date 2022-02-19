299c0db629860401f1bdb8d3f0a009b02ece2b81
Golo_er38@student.itstep.org <Golo_er38@student.itstep.org>
2022-02-19 15:04:56 +0200

    1

--- Lesson_11/Lesson_11.cpp
+++ Lesson_11/Lesson_11.cpp
@@ -1,9 +1,32 @@
 ﻿#include <iostream>
+#include <string>
 #include "SmartPtr.h"
-//#include "SmartPtr.cpp"
+#include "SmartPtr.cpp"
+using namespace std;
+
+class User
+{
+public:
+	User(int id, string name)
+	{
+		_id = id;
+		_name = name;
+		cout << "Constructor:\t" << typeid(this).name() << "\t" << endl;
+	}
+	void SetId (int id) { _id = id; }
+	void SetName (string name) { _name = name; }
+	int GetId() { return _id; }
+	string GetName() { return _name; }
+
+private:
+	int _id;
+	string _name;
+};
 
 int main()
 {
-
+	SmartPtr<User> userSmart (new User(1, "Bob"));
+	cout << userSmart->GetName() << endl;
+	system("pause");
 	return 0;
 }
